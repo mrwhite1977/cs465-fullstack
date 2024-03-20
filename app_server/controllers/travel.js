aaab1460334225f147102b1f5711a27f189e901d
Micheal White <michealwhite77@gmail.com>
2024-03-20 10:40:34 -0400

    Module 3 Completed Baseline

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,3 +1,6 @@
+var fs = require('fs');
+var trips = JSON.parse(fs.readFileSync('./data/trips.json','utf8'));
+
 /* GET travel view */
 const travel = (req, res) => {
     console.log('Inside app_server, controllers, travel.js, travel function.');

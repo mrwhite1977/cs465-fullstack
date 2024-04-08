7fe654a23c5456d21c470fadbf5d9fe784b6eca0
Micheal White <michealwhite77@gmail.com>
2024-04-07 20:35:42 -0400

    Module 5 completed baseline

--- app.js
+++ app.js
@@ -4,10 +4,16 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// Define routers
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
 var travelRouter = require('./app_server/routes/travel');
-var handdlebars = require('hbs');
+var apiRouter = require('./app_api/routes/index');
+
+var handlebars = require('hbs');
+
+// Bring in the database
+require('./app_api/models/db');
 
 var app = express();
 
@@ -15,7 +21,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'app_server', 'views'));
 
 // register handelbars partials (https://npmjs.com.package/hbs)
-handdlebars.registerPartials(__dirname + '/app_server/views/partials');
+handlebars.registerPartials(__dirname + '/app_server/views/partials');
 
 app.set('view engine', 'hbs');
 
@@ -25,9 +31,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Wire-up routes to controllers
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/travel', travelRouter);
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
